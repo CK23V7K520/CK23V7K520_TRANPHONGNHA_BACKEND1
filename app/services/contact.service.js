86e45e12c1d33ed2f58b077233af871d5dc47d26
CK23V7K520 <nhack23v7k520@vlvh.ctu.edu.vn>
2025-03-22 13:26:17 +0700

    Update contact.service.js

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -1,8 +1,35 @@
 const { ObjectId } = require("mongodb");
+
 class ContactService {
-constructor(client) {
-this.Contact = client.db().collection("contacts");
+    constructor(client) {
+        this.Contact = client.db().collection("contacts");
+    }
+
+    //Dinh nghia cac phuong thuc
+    extractConactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+
+        //Remote undefined fields
+        Object.keys(contact).forEach(
+            (key) => contact[key] === undefined && delete contact[key]
+        );
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extractConactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true}
+        );
+    }
 }
-// Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
-}
-module.exports = ContactService;
+
+module.exports = ContactService();
