36e46b81c5ed15dd41ed2ca5c405b4891a6c6866
CK23V7K520 <nhack23v7k520@vlvh.ctu.edu.vn>
2025-03-23 00:26:42 +0700

    Update contact.controller.js

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -50,3 +50,26 @@ exports.create = async (req, res, next) => {
         );
     }
 };
+
+//Retrive all contacts of a user from the database
+exports.findAll = async (req, res, next) => { 
+    let documents = [];
+
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.find({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while retrieving contacts")
+        );
+    }
+    return res.send(documents);
+};
+
+// Find a single contact with an id
