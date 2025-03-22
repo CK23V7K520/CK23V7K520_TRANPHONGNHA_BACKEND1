5a0904fa78cf65b5ba3adb3232b8b34d454f4da4
CK23V7K520 <nhack23v7k520@vlvh.ctu.edu.vn>
2025-03-23 00:26:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
 //start server
-// const POST = config.app.port;
-// app.listen(POST, () => {
-//     console.log(`Server is running on port ${POST}.`);
-//  });
+const POST = config.app.port;
+app.listen(POST, () => {
+    console.log(`Server is running on port ${POST}.`);
+ });
 
-async function startServer() {
-    try {
-        // console.log(config);
-        // console.log(MongoDB);
-        console.log(config.db?.uri);
+// async function startServer() {
+//     try {
+//         // console.log(config);
+//         // console.log(MongoDB);
+//         console.log(config.db?.uri);
 
-        await MongoDB.connect(config.db.uri);
-        console.log("Connect to the database!");
+//         await MongoDB.connect(config.db.uri);
+//         console.log("Connect to the database!");
 
-        const PORT = config.app.port;
-        app.listen(PORT, () => {
-            console.log(`Server connect to the database! ${PORT}`);
-        });
-    } catch (error) {
-        console.log("Cannot connect to the database!", error);
-        process.exit();
-    }
-}
+//         const PORT = config.app.port;
+//         app.listen(PORT, () => {
+//             console.log(`Server connect to the database! ${PORT}`);
+//         });
+//     } catch (error) {
+//         console.log("Cannot connect to the database!", error);
+//         process.exit();
+//     }
+// }
 
-startServer();
+// startServer();
